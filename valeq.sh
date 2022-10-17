b25c5014f5015111935b8496d395d6e5b5d9b846
kultanarz <84865921+kultanarz@users.noreply.github.com>
2022-10-17 18:31:00 +0300

    Add files via upload

---
+++ valeq.sh
@@ -0,0 +1,12 @@
+sudo apt-get install git -y 
+sudo apt-get install golang -y 
+sudo apt-get install perl -y 
+sudo apt-get install python3 -y 
+sudo apt-get install python2 -y 
+sudo apt-get install python3-pip -y 
+sudo apt-get install nodejs -y  
+sudo apt-get install npm -y  
+sudo apt-get install zip -y  
+unzip valeq.zip 
+cd Valeq 
+python3 installer.py  
