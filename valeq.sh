7f4a5092fe22cd80468207238ab8e57780b409f0
kultanarz <84865921+kultanarz@users.noreply.github.com>
2022-10-17 18:56:26 +0300

    Update valeq.sh

--- valeq.sh
+++ valeq.sh
@@ -1,12 +1,12 @@
-apt-get install git -y 
-apt-get install golang -y 
-apt-get install perl -y 
-apt-get install python3 -y 
-apt-get install python2 -y 
-apt-get install python3-pip -y 
-apt-get install nodejs -y  
-apt-get install npm -y  
-apt-get install zip -y  
+sudo apt-get install git -y
+sudo apt-get install golang -y
+sudo apt-get install perl -y
+sudo apt-get install python3 -y
+sudo apt-get install python2 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install nodejs -yapt-get
+sudo apt-get install npm -yapt-get
+sudo apt-get install zip -yapt-get
 unzip valeq.zip 
 cd Valeq 
-python3 installer.py  
+sudo python3 installer.py  
